a0b8c62a0bbdb905718cfc0c851107a6d6e9d8dd
LanSanter <30773991+LanSanter@users.noreply.github.com>
2023-03-25 15:47:52 +0800

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,5 +1,5 @@
 const { Client, GatewayIntentBits } = require('discord.js');
-require('dotenv').config({path:/etc/secrets/Token.env});
+require('dotenv').config();
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
